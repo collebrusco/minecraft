e5ba30374b71cb05fe76c29ee36668699ccbb843
collebrusco <collebrusco.frank@gmail.com>
2025-04-24 17:56:25 -0500

    blanks

---
+++ src/EntityRenderer.cpp
@@ -0,0 +1,4 @@
+#include "EntityRenderer.h"
+
+
+
---
+++ src/EntityRenderer.h
@@ -0,0 +1,11 @@
+/** 
+ * EntityRenderer.h 
+ * minecraft
+ * created 04/24/25 by frank collebrusco
+ */
+#ifndef ENTITY_RENDERER_H
+#define ENTITY_RENDERER_H
+
+
+
+#endif /* ENTITY_RENDERER_H */
